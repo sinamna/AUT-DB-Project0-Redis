81763d34e86e40055cb8973a67c043ba0d061291
sinamna <sina3050@gmail.com>
2021-03-11 15:29:34 +0330

    project done

--- main.py
+++ main.py
@@ -11,4 +11,18 @@ if __name__ == '__main__':
     redisClient = redis.Redis(host='localhost',port=6379)
     handler = RedisHandler(redisClient)
     loadCSV(handler)
-
+    while True:
+        operation = input()
+        operation_list= operation.split(" ")
+        operator=operation_list[0]
+        if operator == "create":
+            handler.set(operation_list[1],operation_list[2])
+        elif operator == "fetch":
+            handler.fetch(operation_list[1])
+        elif operator == "update":
+            handler.update(operation_list[1],operation_list[2])
+        elif operator == "delete":
+            handler.delete(operation_list[1])
+        else:
+            print("you have entered wrong operator")
+        print()
