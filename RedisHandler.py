47604af07908c01e4c8cceff929ef9442ce1c304
sinamna <sina3050@gmail.com>
2021-03-11 11:32:54 +0330

    base of redis handler added

---
+++ RedisHandler.py
@@ -0,0 +1,18 @@
+import redis
+class RedisHandler:
+    """
+    client to handler redis operations
+
+    """
+    def __init__(self,host,port):
+        pass
+
+    # implementing CRUD opetions
+    def create(self):
+        pass
+    def fetch(self):
+        pass
+    def update(self):
+        pass
+    def delete(self):
+        pass
